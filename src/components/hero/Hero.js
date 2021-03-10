415e910584da06e2b91cedac6fdd4fbe5968ed97
Miles Burke <milesj76@gmail.com>
2021-03-10 01:05:10 -0500

    added egjs flicking carousel to hero

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import heroImg1 from '../../img/hero/hero-img1.png'
 import heroImg2 from '../../img/hero/hero-img2.jpg'
 import heroImg3 from '../../img/hero/hero-img3.jpg'
-import testImg from '../../img/counter-thumbnail.PNG'
+import Flicking from '@egjs/react-flicking'
+import {Fade, AutoPlay, Parallax} from '@egjs/flicking-plugins'
 
 const Hero = () => {
     const backgrounds = {
@@ -21,36 +22,45 @@ const Hero = () => {
         }
     }
 
+    let plugins = [new Fade("", 0.8), new AutoPlay(3000, "NEXT")];
+
+    function easeInOutQuint(x) {
+        return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
+        }
+
     const [heroBackground, setHeroBackground] = useState(heroImg2)
 
-    console.log(heroImg2)
-
     return (
-        <section className="hero-container" id="hero"
-        style={{
-            background: `url(${heroBackground})`, backgroundSize: 'cover'
-        }}>
-            <div className="hero-text">
-                <h1>My name is Miles.</h1>
-                <h1>I build responsive websites.</h1>
-            </div>
-            <div className="hero-btns-row">
-                <a href="#projects" className="hero-btn">View Portfolio</a>
-                <a href="#contact" className="hero-btn">Hire Me</a>
-            </div>
-            <div className="hero-carousel">
-                <svg id="carousel1" width="8" height="8">
-                    <rect width="8" height="8" />
-                </svg>
-                <svg id="carousel2" width="8" height="8">
-                    <rect width="8" height="8" />
-                </svg>
-                <svg id="carousel3" width="8" height="8">
-                    <rect width="8" height="8" />
-                </svg>
-            </div>
+
+        <section className="flicking-wrapper">
+            <Flicking
+                duration={3000}
+                circular={true}
+                gap={0}
+                adaptive={true}
+                panelEffect={x => easeInOutQuint(x)}
+                autoResize={true}
+                plugins={plugins}
+            >
+                <div className="flicking-panel" style={{ backgroundImage: "url("+heroImg1+")"}} />
+                <div className="flicking-panel hero-filter" style={{ backgroundImage: "url("+heroImg2+")"}} />
+                <div className="flicking-panel hero-filter" style={{ backgroundImage: "url("+heroImg3+")"}} />
+            </Flicking>
+            <section className="hero-container" id="hero">
+                <div className="hero-box">
+                    <div className="hero-text">
+                        <h1>My name is Miles.</h1>
+                        <h1>I build responsive websites.</h1>
+                    </div>
+                    <div className="hero-btns-row">
+                        <a href="#projects" className="hero-btn">View Portfolio</a>
+                        <a href="#contact" className="hero-btn">Hire Me</a>
+                    </div>
+                </div>
+            </section>
         </section>
-    )
+        
+    );
 }
 
 export default Hero
